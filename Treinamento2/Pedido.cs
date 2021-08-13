e6cd36e7f1e6e317f2b2e4acf6e18a0fb00fb268
luiz fernando silva <luiz_santa@hotmail.com>
2021-08-13 12:47:05 -0300

    Adicionar arquivos de projeto.

---
+++ Treinamento2/Pedido.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treinamento2
+{
+    class Pedido
+
+    {
+
+      
+        public int Hamburguer { get; set; }
+
+        public double preco { get; set; }
+
+
+        public  double ValorTotalPedido()
+            {
+            return Hamburguer * preco;
+            }
+        public void AdicionarProdutos(int qte)
+        {
+            Hamburguer += qte;
+        }
+        public override string ToString()
+        {
+            return
+
+                 " Valor total pedido "
+                 
+                + ValorTotalPedido().ToString("f2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Treinamento2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Treinamento2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pedido p = new Pedido();
+
+            p.preco = 8.00;
+
+            string nome;
+            
+
+            Console.WriteLine("Quantos Hamburguers deseja?");
+
+            p.Hamburguer = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("o total do seu pedido é " + p);
+
+            Console.WriteLine("Deseja pedir mais hamuburguer?");
+            Console.WriteLine("S / N");
+            nome = Console.ReadLine();
+            
+            while (nome.ToUpper() != "N")
+            {
+                Console.WriteLine("Digite a quantidade de hamburguer a adicionar ao seu pedido");
+                int qte = int.Parse(Console.ReadLine());//o que for digitado sera armazenado na variavel qte
+
+                
+
+                Console.WriteLine("Deseja pedir mais hamuburguer?");
+                Console.WriteLine("S / N");
+                p.AdicionarProdutos(qte);
+                
+                nome = Console.ReadLine();
+                
+
+               
+
+            }
+
+            Console.WriteLine("o total do seu pedido é " + p);
+        }
+    }
+}
